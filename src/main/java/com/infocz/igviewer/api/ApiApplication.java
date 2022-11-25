471d7ff4be0987acae4fcc78ca9106e17939f7e5
justic <justic@infocz.co.kr>
2022-11-25 10:54:26 +0900

    war build  수정

--- src/main/java/com/infocz/igviewer/api/ApiApplication.java
+++ src/main/java/com/infocz/igviewer/api/ApiApplication.java
@@ -2,12 +2,25 @@ package com.infocz.igviewer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class ApiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+// public class ApiApplication {
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(ApiApplication.class, args);
+// 	}
+// }
+
+public class ApiApplication extends SpringBootServletInitializer {	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
