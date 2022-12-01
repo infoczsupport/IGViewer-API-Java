b50a00783ea2ef16f3412ec7a32eef5cc185a859
justic <justic@infocz.co.kr>
2022-12-01 11:20:32 +0900

    수정

--- src/main/java/com/infocz/igviewer/api/servive/gdb/CustomUserDetailsService.java
+++ src/main/java/com/infocz/igviewer/api/servive/gdb/CustomUserDetailsService.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
