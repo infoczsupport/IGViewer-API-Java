27b0e731e1b8ed132c6a7f3e7ac086b4de6fea5e
justic <justic@infocz.co.kr>
2022-12-06 13:03:13 +0900

    수정

--- src/main/java/com/infocz/igviewer/api/servive/gdb/CustomUserDetailsService.java
+++ src/main/java/com/infocz/igviewer/api/servive/gdb/CustomUserDetailsService.java
@@ -14,8 +14,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpUtils;
-
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -49,7 +47,7 @@ public class CustomUserDetailsService implements UserDetailsService{
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String loginId){
-        // getNdopRequest(loginId);
+        getLdopRequest(loginId);
         Map<String, Object> userInfo = users.stream()
             .filter(m -> Objects.equals(m.get("loginId"), loginId))
             .findFirst()
@@ -81,21 +79,21 @@ public class CustomUserDetailsService implements UserDetailsService{
                       .authorities(grantedAuthorities).build();
     }
 
-    private String getNdopRequest(String id, String pwd) {
-		String url = "http://192.168.0.2:9201/twitter/_doc/1";
-        String params = "user";
-		String method = "GET";
+    // private String getLdopRequest(String id, String pwd) {
+	// 	String params = "user=" + id + "&pwd=" + pwd;
+    private String getLdopRequest(String id) {
+		String params = "user=" + id;
+		String url = "http://localhost:3000/robots.txt?" + params;
+        String method = "GET";
 		String result = "";
 		HttpURLConnection conn = null;
 		
         //HttpURLConnection 객체 생성
 		conn = getHttpURLConnection(url, method);
-		//URL 연결에서 데이터를 읽을지에 대한 설정 ( defualt true )
-        // conn.setDoInput(true); 
 		//API에서 받은 데이터를 StringBuilder 형태로 리턴하여 줍니다. 
         result = getHttpRespons(conn);
         //해당 정보를 확인합니다.
-		System.out.println("GET = " + result);
+		log.debug("GET getLdopRequest = " + result);
         return result;
 	}
 
@@ -124,12 +122,9 @@ public class CustomUserDetailsService implements UserDetailsService{
 		StringBuilder sb = null;
 
 		try {
-			if(conn.getResponseCode() == 200) {
-            // 정상적으로 데이터를 받았을 경우
-            	//데이터 가져오기
+			if(conn.getResponseCode() == 200) { // 정상적으로 데이터를 받았을 경우
 				sb = readResopnseData(conn.getInputStream());
-			}else{
-            // 정상적으로 데이터를 받지 못했을 경우            
+			}else{								// 정상적으로 데이터를 받지 못했을 경우            
             	//오류코드, 오류 메시지 표출
 				System.out.println(conn.getResponseCode());
 				System.out.println(conn.getResponseMessage());
