eeca86be4986282c79a8321ea127ef03979000bf
justic <justic@infocz.co.kr>
2023-01-02 12:59:20 +0900

    spring security 수정

--- src/main/java/com/infocz/igviewer/api/config/SecurityConfig.java
+++ src/main/java/com/infocz/igviewer/api/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.infocz.igviewer.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -18,7 +17,7 @@ import com.infocz.igviewer.api.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -36,8 +35,7 @@ public class SecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(){
-        return (web) -> web.ignoring()
-            .antMatchers("/favicon.ico");
+        return (web) -> web.ignoring().requestMatchers("/favicon.ico");
     }
 
     @Bean
@@ -56,16 +54,15 @@ public class SecurityConfig {
 
 					/** HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정*/
                     .and()
-                    .authorizeRequests()
-                    .antMatchers("/com/**"
-                        , "/api/cypher/**"
-                        , "/api/rdb2/**"
-                        , "/api/getDbMonitor").permitAll()
-                    // .antMatchers("/api/rdb/getTableData").hasAuthority("converter")   
-					.anyRequest().authenticated()
-
+                    .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/com/**"
+                            , "/api/cypher/**"
+                            , "/api/rdb2/**"
+                            , "/api/getDbMonitor").permitAll()
+                        // .requestMatchers("/api/user/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                    )
                     /**JwtSecurityConfig 적용 */
-                    .and()
                     .apply(new JwtSecurityConfig(tokenProvider))
 
                     .and().build();
--- src/main/java/com/infocz/igviewer/api/servive/ndap/NdapService.java
+++ src/main/java/com/infocz/igviewer/api/servive/ndap/NdapService.java
@@ -11,7 +11,6 @@ import org.springframework.boot.json.JsonParseException;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.log4j.Log4j2;
-import net.bitnine.agensgraph.deps.org.json.simple.JSONObject;
 
 @Log4j2
 @Service
